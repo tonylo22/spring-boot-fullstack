168e88151eaac1a88de1ab889301a1efac2005c7
Tony <devtonyhk@gmail.com>
2024-03-04 01:23:05 +0800

    Update test controller for cd workflow

--- backend/src/main/java/com/example/PingPongController.java
+++ backend/src/main/java/com/example/PingPongController.java
@@ -9,6 +9,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("pong");
+        return new PingPong("pong!");
     }
 }
